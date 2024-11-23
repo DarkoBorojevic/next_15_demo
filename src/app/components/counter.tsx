6a5c0b4dce4dddebcf460c2e4795317dc9d4188d
DarkoBorojevic <info@tipoit.net>
2024-11-23 18:38:49 +0100

    Adding a counter component (client component demonstration).

---
+++ src/app/components/counter.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { useState } from 'react'
+
+const Counter = () => {
+    console.log("counter component!")
+  const [count, setCount] = useState(0);
+  return (
+    <button onClick={() => setCount(count + 1)}>
+        clicked {count} times
+    </button>
+  )
+}
+
+export default Counter
