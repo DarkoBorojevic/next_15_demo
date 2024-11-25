fc8c92b27e4ff5aaa4b36a2562b7ea08c00c31aa
DarkoBorojevic <info@tipoit.net>
2024-11-25 09:58:49 +0100

    Implementing navigation and useRouter hook.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,21 @@
+'use client';
+
+import { useRouter } from "next/navigation";
+
 const About = () => {
+  const router = useRouter();
+
   return (
-    <div>About</div>
+    <>
+      <div>About</div>
+      <button
+        onClick={() => router.push("/")}  
+        className="bg-blue-500 text-white p-2 rounded-md"
+      >
+        go Home
+      </button>
+    </>
+    
   )
 }
 
